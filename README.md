35d79d197fcd6e4f49d200f6c1ae58af0c6d5dcc
Кискин Дмитрий Александрович <d_kiskin@r-t.local>
2026-08-03 16:17:53 +0300

    Initial project structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# SQL E-commerce Analysis
+
+Портфолио-проект по анализу продаж интернет-магазина.
+
+## Используемые технологии
+
+- PostgreSQL
+- DBeaver
+- SQL
+- Git
+- VS Code
---
+++ sql/01_create_database.sql
@@ -0,0 +1,9 @@
+-- =====================================
+-- Project: SQL Ecommerce Analysis
+-- File: 01_create_database.sql
+-- Author: Дмитрий
+-- =====================================
+
+-- Database:
+-- ecommerce
+CREATE DATABASE ecommerce;
